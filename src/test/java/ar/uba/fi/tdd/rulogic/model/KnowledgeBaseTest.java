1d48e64401d3d8134c7bd82ac5cb5c13a1c52b98
guidonegri <bokita_93@hotmail.com>
2017-11-05 01:22:53 -0300

    Add some test

--- src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
@@ -19,11 +19,46 @@ public class KnowledgeBaseTest {
 	}
 
 	@Test
-	public void test() {
+	public void test_valid_query_varon() {
 		KnowledgeBase kb = new KnowledgeBase();
 		kb.parseDB("src/main/resources/rules.db");
-		Assert.assertTrue(this.knowledgeBase.answer("varon (javier)."));
-
+		Assert.assertTrue(this.knowledgeBase.answer("varon(juan)."));
 	}
-
+	
+	@Test
+	public void test_valid_query_mujer() {
+		KnowledgeBase kb = new KnowledgeBase();
+		kb.parseDB("src/main/resources/rules.db");
+		Assert.assertTrue(this.knowledgeBase.answer("mujer(cecilia)."));
+	}
+	
+	@Test
+	public void test_valid_query_two_params() {
+		KnowledgeBase kb = new KnowledgeBase();
+		kb.parseDB("src/main/resources/rules.db");
+		Assert.assertTrue(this.knowledgeBase.answer("padre(juan, pepe)."));
+	}
+	
+	@Test
+	public void test_invalid_query_format() {
+		KnowledgeBase kb = new KnowledgeBase();
+		kb.parseDB("src/main/resources/rules.db");
+		Assert.assertFalse(this.knowledgeBase.answer("varon (juan) ."));
+	}
+	
+	@Test
+	public void test_no_match_query_one_param() {
+		KnowledgeBase kb = new KnowledgeBase();
+		kb.parseDB("src/main/resources/rules.db");
+		Assert.assertFalse(this.knowledgeBase.answer("varon(maria)."));
+	}
+	
+	
+	@Test
+	public void test_no_match_query_two_params() {
+		KnowledgeBase kb = new KnowledgeBase();
+		kb.parseDB("src/main/resources/rules.db");
+		Assert.assertFalse(this.knowledgeBase.answer("hermano(alfredo, roberto)."));
+	}	
+	
 }
